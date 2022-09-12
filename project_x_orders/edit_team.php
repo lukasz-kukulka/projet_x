5ee79c6397fb9e4b9fcfe45f091bb4d1e7d37372
Lukasz Kukulka <qqla83@gmail.com>
2022-09-12 19:03:53 +0100

    edit team init

--- project_x_orders/edit_team.php
+++ project_x_orders/edit_team.php
@@ -1,5 +1,18 @@
 <?php
     function editTeam() {
         echo "Edytuj drużynę";
+
+    }
+    $results;
+    function readRecord() {
+        $user = wp_get_current_user()->display_name;
+        echo '<form method="post">';
+        echo '<br /><br />Nazwa drużyny<input type="text" name="team" value=""/> <br />';
+        echo '<br /><br />Nazwa klubu<input type="text" name="club" value="" /> <br />';
+        echo '<br /><br />Podaj imie i nazwisko trenera<input type="text" name="trener" value="'.$user.'"/> <br />';
+        echo '<br /><br />Podaj imie i nazwisko menagera<input type="text" name="manager" value="'.$user.'"/> <br />';
+        echo '<br /><br />kierownika<input type="text" name="director" value="'.$user.'"/> <br />';
+		echo '<input type="submit" name="confirm" class="button" value = "Dodaj"/>';
+	    echo '</form>';
     }
 ?>
